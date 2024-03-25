8d69c415ed0ff28acc0aaa6bf5713130f64e31d4
Sanjay G <sanjaysanju7560@gmail.com>
2024-03-25 13:54:13 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:HomeComponent,path:''
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+// const routes: Routes = [
+// {
+//   component:LoginComponentComponent,path:''
+// },
+// {
+//   component:DashboardComponentComponent,path:'dashboard'
+// }
+// ];
